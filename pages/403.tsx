f5986cc561d099cc82b219769bb08aabd057c725
dangphu2412 <dangphu241299@gmail.com>
2023-08-19 18:10:45 +0700

    fix: fix build

--- pages/403.tsx
+++ pages/403.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NoLayout } from 'src/system/app/internal/components/NoLayout';
-import { NextPageWithLayout } from './_app';
+import { NextPageWithLayout } from '../src/system/infrastructure/next.types';
 
 const MaintenancePage: NextPageWithLayout = (): React.ReactElement => (
   <>403: Abort access</>
