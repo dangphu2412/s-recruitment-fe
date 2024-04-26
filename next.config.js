eea28b1ea607c000c53f3a2af133742faed09872
dangphu2412 <dangphu241299@gmail.com>
2024-04-26 17:46:39 +0700

    feature/upgrade-nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js'
+        }
+      }
+    }
+  },
   eslint: {
     dirs: ['src'],
     ignoreDuringBuilds: true
