db229c99c8fd9b23cb2f573c1a77ec50fc13b141
dangphu2412 <dangphu241299@gmail.com>
2024-06-09 22:24:14 +0700

    feat: build document view

--- src/features/documents/document-overviews/ui/document-overview.tsx
+++ src/features/documents/document-overviews/ui/document-overview.tsx
@@ -1,3 +1,45 @@
+import { DocumentCard } from './DocumentCard';
+
 export function DocumentOverview() {
-  return <>Hello Overview</>;
+  const items = [
+    {
+      category: 'Nội Quy',
+      title: 'Nội Quy S-Group',
+      titleSlug: 'noi-quy-s-group',
+      author: 'Đặng Ngọc Phú',
+      image: '/rules.jpeg',
+      imageAlt: 'rules'
+    },
+    {
+      category: 'Tin tức',
+      title: 'Chuyển văn phòng',
+      titleSlug: 'chuyen-van-phong',
+      author: 'Đặng Ngọc Phú'
+    },
+    {
+      category: 'Tin tức',
+      title: 'Unihack 2024',
+      titleSlug: 'unihack-2024',
+      author: 'Đặng Ngọc Phú'
+    }
+  ];
+  const hotTopic = items[0];
+  const remains = items.slice(1);
+
+  return (
+    <div className="flex flex-row space-x-12">
+      <DocumentCard {...hotTopic} isHot />
+
+      <div className="flex flex-col">
+        {remains.map(item => {
+          return (
+            <div>
+              <DocumentCard key={item.category} {...item} />
+              <hr />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
