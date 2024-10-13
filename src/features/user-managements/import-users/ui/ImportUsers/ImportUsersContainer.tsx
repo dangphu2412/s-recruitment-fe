e65f1a65a2adc6dba4a5e790bd0c11b9d9dcb128
dangphu2412 <dangphu241299@gmail.com>
2024-10-13 12:02:16 +0700

    feat: update import users

--- src/features/user-managements/import-users/ui/ImportUsers/ImportUsersContainer.tsx
+++ src/features/user-managements/import-users/ui/ImportUsers/ImportUsersContainer.tsx
@@ -1,65 +1,29 @@
-import React, { ChangeEvent, useRef } from 'react';
-import { Button, Input } from '@chakra-ui/react';
+import React from 'react';
+import { Button, useDisclosure } from '@chakra-ui/react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUpload } from '@fortawesome/free-solid-svg-icons';
-import {
-  QUERY_USERS_KEY,
-  useMutateUploadUserByFile
-} from '../../../../../entities/user/models';
-import { useNotify } from '../../../../../shared/models/notify';
-import { useQueryClient } from 'react-query';
+import { ImportUsersDrawer } from './ImportUsersModal';
 
 export function ImportUsersContainer(): React.ReactElement {
-  const ref = useRef<HTMLInputElement>(null);
-  const { uploadUserByFile } = useMutateUploadUserByFile();
-  const notify = useNotify();
-  const queryClient = useQueryClient();
-
-  function handleImport(e: ChangeEvent<HTMLInputElement>) {
-    const file = e.target.files?.[0];
-
-    if (!file) {
-      return;
-    }
-
-    uploadUserByFile(
-      {
-        file,
-        fieldMappings: JSON.stringify({
-          'Họ và Tên:': 'fullName',
-          Email: 'email',
-          Username: 'username'
-        })
-      },
-      {
-        onSuccess: () => {
-          notify({
-            title: 'Import successfully',
-            status: 'success'
-          });
-          queryClient.invalidateQueries({
-            queryKey: [QUERY_USERS_KEY]
-          });
-        },
-        onError: () => {
-          notify({
-            title: 'Import failed',
-            description: 'Format should be: Họ và Tên, Email, Username',
-            status: 'error'
-          });
-        }
-      }
-    );
-  }
+  const btnRef = React.useRef<HTMLButtonElement>(null);
+  const { isOpen, onOpen, onClose } = useDisclosure({
+    id: 'ImportDrawer'
+  });
 
   return (
     <>
-      <Button colorScheme="pink" onClick={() => ref?.current?.click()}>
+      <Button colorScheme="pink" onClick={onOpen} ref={btnRef}>
         <FontAwesomeIcon className="mr-2" icon={faUpload} />
         <span>Import</span>
       </Button>
 
-      <Input type={'file'} hidden ref={ref} onChange={handleImport} />
+      {isOpen && (
+        <ImportUsersDrawer
+          isOpen={isOpen}
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        />
+      )}
     </>
   );
 }
