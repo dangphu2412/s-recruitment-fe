b5b65fef7db3fcca45eaa5394623c722feb2395b
dangphu2412 <dangphu241299@gmail.com>
2025-07-01 16:25:16 +0700

    feat: migrate activity trends to dashboard api

--- src/entities/dashboard/api/dashboard-api.client.ts
+++ src/entities/dashboard/api/dashboard-api.client.ts
@@ -1,4 +1,5 @@
 import { authorizedHttpClient } from '../../../shared/api';
+import { GroupType } from '../config/dashboard.constants';
 
 export type KPI = {
   totalPayment: number;
@@ -7,11 +8,31 @@ export type KPI = {
   totalLateMembers: number;
 };
 
+export type UserActivityTrendResponse = {
+  items: UserActivityTrend[];
+};
+
+export type UserActivityTrend = {
+  lateCount: number;
+  onTimeCount: number;
+  notFinishedCount: number;
+  date: string;
+};
+
 export const dashboardApiClient = {
   getKPI: () => {
     return authorizedHttpClient.request<KPI>({
       url: '/dashboard/kpi',
       method: 'get'
+    });
+  },
+  getUserActivityTrends: (groupType: GroupType) => {
+    return authorizedHttpClient.request<UserActivityTrendResponse>({
+      url: '/dashboard/user-activity-trends',
+      method: 'get',
+      params: {
+        groupType
+      }
     });
   }
 };
--- src/entities/dashboard/models/dashboard.model.ts
+++ src/entities/dashboard/models/dashboard.model.ts
@@ -1,9 +1,17 @@
 import { useQuery } from 'react-query';
 import { dashboardApiClient } from '../api/dashboard-api.client';
+import { GroupType } from '../config/dashboard.constants';
 
 export function useDashboardKPI() {
   return useQuery({
     queryKey: ['dashboard-kpi'],
     queryFn: dashboardApiClient.getKPI
+  });
+}
+
+export function useDashboardUserActivityTrend(groupType: GroupType) {
+  return useQuery({
+    queryKey: ['dashboard-kpi', groupType],
+    queryFn: () => dashboardApiClient.getUserActivityTrends(groupType)
   });
 }
